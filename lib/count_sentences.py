721c4453a43d94a3bb4b4b6149e54bf636baca08
bettyje <bettyjelagat156@gmail.com>
2024-12-02 15:17:28 +0300

    defining properties and instance methods on a class

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if not isinstance(value, str):
+            raise ValueError("Value must be a string")
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        import re
+        sentences = re.split(r'[.!?]', self.value)
+        sentences = [sentence.strip()
+                     for sentence in sentences if sentence.strip()]
+        return len(sentences)
